be9c1985d4ce4fde9427b30bdf89e7a17dff4c7e
Lydia de Aquino <lydiaaquino83@gmail.com>
2026-08-25 19:54:37 -0300

    primeiro commit

---
+++ src/test/java/kl/gestaoEscolar/GestaoEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package kl.gestaoEscolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
