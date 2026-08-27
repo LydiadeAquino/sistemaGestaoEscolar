673cc1a5cade68637a90456de2db8394b29b229e
Karolinie Arruda Galvão <karoliniearrudagalvao@gmail.com>
2026-08-27 16:51:40 -0300

    Adição Aluno e Professor

--- src/main/java/kl/gestaoEscolar/entities/Aluno.java
+++ src/main/java/kl/gestaoEscolar/entities/Aluno.java
@@ -1,4 +1,19 @@
 package kl.gestaoEscolar.entities;
 
-public class Aluno {
+import jakarta.persistence.Entity;
+import kl.gestaoEscolar.enums.Perfil;
+
+@Entity
+public class Aluno extends Usuario {
+
+    //construtor
+    public Aluno(){
+
+    }
+
+    public Aluno (Long id, String nome, String email, String senha, Perfil perfil){
+        super(id,nome,email,senha,perfil);
+
+    }
+
 }
--- src/main/java/kl/gestaoEscolar/entities/Professor.java
+++ src/main/java/kl/gestaoEscolar/entities/Professor.java
@@ -1,4 +1,33 @@
 package kl.gestaoEscolar.entities;
 
-public class Professor {
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import kl.gestaoEscolar.enums.Perfil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+
+public class Professor extends Usuario {
+
+    @OneToMany(mappedBy = "professor")// O QUE DIZ QUE UM PROFESSOR PODE TER MUITAS DISCIPLINAS. MAPEAMENTO PELO "PROFESSOR"
+    private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+
+    //Construtor
+    public Professor(){
+
+    }
+
+    public Professor (Long id, String nome, String email,String senha, Perfil perfil){
+        super(id,nome,email,senha,perfil);
+    }
+    //GET E SET
+
+    public List<Disciplina> getDisciplinas(){
+        return disciplinas;
+    }
+    public void setDisciplinas(List<Disciplina> disciplinas ){
+        this.disciplinas=disciplinas;
+    }
 }
